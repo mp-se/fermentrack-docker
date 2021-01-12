bec7f36c3c34a6cf63fff02408e566eb2b1221e1
Magnus <magnus@noreply.github.com>
2021-01-12 20:21:11 +0100

    Testing autobuild in github

--- fermentrack/entrypoint.sh
+++ fermentrack/entrypoint.sh
@@ -67,14 +67,14 @@ EOF
 # Setting priviligies required for bluetooth support. If we cannot start python we are not running in priviligied mode.
 #
 echo "Checking if the container is running in priviliged mode"
-setcap cap_net_raw,cap_net_admin+eip /usr/bin/python3.8
+setcap cap_net_raw,cap_net_admin+eip /usr/bin/python3.7
 if python3 -h > /dev/null;
 then
     echo "Container is running in priviligied mode"
 else 
     echo "Container is NOT running in priviligied mode"
     # We need to remove the setcap options or python will fail to start fermentrack
-    setcap -r /usr/bin/python3.8
+    setcap -r /usr/bin/python3.7
 fi
 
 #
